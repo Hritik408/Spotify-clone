fb7c3b5166d1ea1467f9aac3fb76331330c84c3e
Hritik408 <chaudharyhritik91@gmail.com>
2024-09-07 02:04:58 +0530

    ..

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,32 @@
+import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
+
+// const options = {
+//   method: 'GET',
+//   headers: {
+//     'x-rapidapi-key': '541eaaa774msh3f4f2e158faf03cp1e9f9fjsn4478c14cfaae',
+//     'x-rapidapi-host': 'shazam.p.rapidapi.com'
+//   }
+// };
+
+// fetch('https://shazam.p.rapidapi.com/charts/list', options)
+//   .then(response => response.json())
+//   .then(response => console.log(response))
+//   .catch(err => console.error(err));
+
+
+    export const shazamCoreApi = createApi({
+      reducerPath: 'shazamCoreApi',
+      baseQuery: fetchBaseQuery({
+        baseUrl: 'https://shazam.p.rapidapi.com',
+        prepareHeaders:  (headers) => {  // prepare headers before each and every api
+          headers.set('x-rapidapi-key','541eaaa774msh3f4f2e158faf03cp1e9f9fjsn4478c14cfaae');
+
+          return headers;
+        }
+      }),
+      endpoints: (builder) => ({
+        getTopCharts: builder.query({ query: ( ) => '/charts/list'})
+      })
+     });
+
+     export const { useGetTopChartsQuery, } = shazamCoreApi;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,15 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore,} from '@reduxjs/toolkit';
 
 import playerReducer from './features/playerSlice';
+import { shazamCoreApi } from './services/shazamCore';
 
 export const store = configureStore({
   reducer: {
+    [shazamCoreApi.reducerPath]: shazamCoreApi.reducer,
     player: playerReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(shazamCoreApi.middleware),
 });
+
+
+// Middleware in Redux is used to extend the store's capabilities, such as handling asynchronous actions or logging actions. Here’s a breakdown of how you can use it:
